8127c77f0b5509065c24592d4e57e6465382880f
shantoux <sandra_95170@hotmail.com>
2021-03-25 16:40:04 +0100

    création fichier

--- src/ARN.java
+++ src/ARN.java
@@ -1,3 +1,22 @@
 public class ARN {
+    private String sequence;
+    private String appariements;
+
+    public ARN(String sequence, String appariements){
+        this.sequence = sequence;
+        this.appariements = appariements;
+    }
+
+    @Override
+    public String toString(){
+        return(" " + this.sequence + "\n " + this.appariements);
+    }
+
+    public static void main(String[] args) {
+        ARN l = new ARN("AUGCAUUTG", "((-))----");
+        System.out.println(l);
+
+    }
 
 }
+
