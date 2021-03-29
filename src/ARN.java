38b30e9051a241ddd14098836622a3b4352d83f1
shantoux <sandra_95170@hotmail.com>
2021-03-29 12:14:13 +0200

    Correction des erreurs

--- src/ARN.java
+++ src/ARN.java
@@ -1,6 +1,6 @@
 public class ARN {
-    private String sequence;
-    private String appariements;
+    private final String sequence;
+    private final String appariements;
 
     //CONSTRUCTEURS
 
@@ -14,40 +14,46 @@ public class ARN {
         return(" " + this.sequence + "\n " + this.appariements);
     }
 
-    public boolean meme_forme(ARN arn) {
-        int i = 0;
-        int j = 0;
-        boolean res = true;
-        while (this.appariements.charAt(i) == '-') i++;
-        while (arn.appariements.charAt(j) == '-') j++;
-        while (i<this.appariements.length() && j<arn.appariements.length()){
-          if (this.appariements.charAt(i) == arn.appariements.charAt(j)){
-            i++;
-            j++;
-          }
-          else {
-            return false;
-          }
+    public boolean egalite(ARN arn, String methode){
+        boolean res = false;
+        if (methode.equals("forme")){
+            res = this.appariements.equals(arn.appariements);
+        }
+        else if (methode.equals("sequence")){
+            res = (this.sequence.equals(arn.sequence)&&this.appariements.equals(arn.appariements));
         }
         return res;
     }
 
-    public boolean meme_sequence(ARN arn) {
+
+    public boolean comparaison(ARN arn, String methode) {
         int i = 0;
         int j = 0;
-        boolean res = true;
         while (this.appariements.charAt(i) == '-') i++;
         while (arn.appariements.charAt(j) == '-') j++;
-        while (i<this.appariements.length() && j<arn.appariements.length()){
-            if (this.sequence.charAt(i) == arn.sequence.charAt(j)){
-                i++;
-                j++;
-            }
-            else {
-                return false;
+        if (methode.equals("forme")){
+            while (i<this.appariements.length() && j<arn.appariements.length()){
+                if (this.appariements.charAt(i) == arn.appariements.charAt(j)){
+                    i++;
+                    j++;
+                }
+                else {
+                    return false;
+                }
             }
         }
-        return res;
+        else if (methode.equals("sequence")){
+            while (i<this.appariements.length() && j<arn.appariements.length()){
+                if (this.sequence.charAt(i) == arn.sequence.charAt(j)){
+                    i++;
+                    j++;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
@@ -56,10 +62,14 @@ public class ARN {
         System.out.println(l);
         ARN l2 = new ARN("UUUAUGCAUUTG", "---((-))----");
         ARN l3 = new ARN("UUAUGCAGUTG", "--(((---)))");
-        System.out.println(l.meme_forme(l2));
-        System.out.println(l.meme_sequence(l2));
-        System.out.println(l.meme_forme(l3));
-        System.out.println(l.meme_sequence(l3));
+        ARN l4 = new ARN("AAAAAUGCAGUTG", "----((-))----");
+
+        System.out.println(l.comparaison(l2, "forme"));
+        System.out.println(l.comparaison(l2, "sequence"));
+        System.out.println(l.comparaison(l3, "forme"));
+        System.out.println(l.comparaison(l3, "sequence"));
+        System.out.println(l.egalite(l4, "forme"));
+        System.out.println(l.egalite(l4,"sequence"));
     }
 
 
