973b71125182760dcb5fec05267b38194e6e1b96
shantoux <sandra_95170@hotmail.com>
2021-03-29 11:00:47 +0200

    Update ARN.java
    méthode sur ARN pour comparer deux appariements

--- src/ARN.java
+++ src/ARN.java
@@ -14,13 +14,34 @@ public class ARN {
         return(" " + this.sequence + "\n " + this.appariements);
     }
 
+    public boolean meme_forme(ARN arn) {
+        int i = 0;
+        int j = 0;
+        res = true;
+        while this.appariements.charAt(i) != "("{
+          i++;
+        }
+        while arn.appariements.charAt(j) != "("{
+          j++;
+        }
+        while (i<this.appariements.length() && j<arn.appariements.length()){
+          if (this.appariements.charAt(i) == arn.appariements.charAt(j)){
+            i++;
+            j++;
+          }
+          else {
+            return false;
+          }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         ARN l = new ARN("AUGCAUUTG", "((-))----");
         System.out.println(l);
 
     }
 
-    
+
 
 }
-
