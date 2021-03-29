2c68e846c9b2ab1f741d5f33e51491a17791b4dd
shantoux <sandra_95170@hotmail.com>
2021-03-29 11:29:14 +0200

    Méthode comparer les séquences

--- src/ARN.java
+++ src/ARN.java
@@ -17,13 +17,9 @@ public class ARN {
     public boolean meme_forme(ARN arn) {
         int i = 0;
         int j = 0;
-        res = true;
-        while this.appariements.charAt(i) != "("{
-          i++;
-        }
-        while arn.appariements.charAt(j) != "("{
-          j++;
-        }
+        boolean res = true;
+        while (this.appariements.charAt(i) == '-') i++;
+        while (arn.appariements.charAt(j) == '-') j++;
         while (i<this.appariements.length() && j<arn.appariements.length()){
           if (this.appariements.charAt(i) == arn.appariements.charAt(j)){
             i++;
@@ -36,10 +32,34 @@ public class ARN {
         return res;
     }
 
-    public static void main(String[] args) {
-        ARN l = new ARN("AUGCAUUTG", "((-))----");
-        System.out.println(l);
+    public boolean meme_sequence(ARN arn) {
+        int i = 0;
+        int j = 0;
+        boolean res = true;
+        while (this.appariements.charAt(i) == '-') i++;
+        while (arn.appariements.charAt(j) == '-') j++;
+        while (i<this.appariements.length() && j<arn.appariements.length()){
+            if (this.sequence.charAt(i) == arn.sequence.charAt(j)){
+                i++;
+                j++;
+            }
+            else {
+                return false;
+            }
+        }
+        return res;
+    }
 
+
+    public static void main(String[] args) {
+        ARN l = new ARN("AAAAAUGCAGUTG", "----((-))----");
+        System.out.println(l);
+        ARN l2 = new ARN("UUUAUGCAUUTG", "---((-))----");
+        ARN l3 = new ARN("UUAUGCAGUTG", "--(((---)))");
+        System.out.println(l.meme_forme(l2));
+        System.out.println(l.meme_sequence(l2));
+        System.out.println(l.meme_forme(l3));
+        System.out.println(l.meme_sequence(l3));
     }
 
 
