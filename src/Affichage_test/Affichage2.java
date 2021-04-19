ec1f7851e6f2e1ae740e12efc75cab511c586bbc
Anthony Boutard <tony44wind@gmail.com>
2021-04-19 18:40:38 +0200

    Tests Affichage2

--- src/Affichage_test/Affichage2.java
+++ src/Affichage_test/Affichage2.java
@@ -11,38 +11,42 @@ public class Affichage2 {
         JFrame fenetre = new JFrame("Manipulation de structures secondaires");
         JLabel description = new JLabel("Ce programme vous propose de manipuler des séquences ADN et de les comparer entre elles au niveau de leurs structures secondaires", JLabel.CENTER);
 
-        final JTextArea Arn1 = new JTextArea("Entrez ici le premier ARN choisi");
-        final JTextArea Arn2 = new JTextArea("Entrez ici le second ARN choisi");
-        Arn1.setBounds(10, 50, 180, 20);
-        Arn2.setBounds(200, 240, 180, 20);
+        final JTextField Arn1 = new JTextField("Entrez ici le premier ARN choisi");
+        final JTextField Arn2 = new JTextField("Entrez ici le second ARN choisi");
+        final JTextField resultat = new JTextField();
+        Arn1.setBounds(20,40,800,880);
+        Arn2.setBounds(500,40,600,880);
 
         //Definir le panel
         JPanel caseArn = new JPanel();
-        caseArn.setBounds(40,50,150,150);
+        caseArn.setBounds(50,50,800,800);
         caseArn.setBackground(Color.lightGray);
 
         //Ajouter un bouton "comparer"
         JButton comparer = new JButton("Comparer les séquences");
         comparer.setBackground(Color.GREEN);
-        comparer.setBounds(50,100,80,30);
+        comparer.setBounds(320,850,300,30);
 
         //Ajouter les zones de text des deux ARN à comparer
-
+        caseArn.add(Arn1);
+        caseArn.add(Arn2);
 
         //Ajouter les commentaires de text à l'interieur des zones de textes
 
-        //Ajouter le bouton et les commentaires au panel
-        fenetre.setLayout(new GridLayout(10, 10));
+        comparer.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                resultat.setText("Voilà ce que donne la comparaison !");
+            }
+        });
 
+        //fenetre.setLayout(new GridLayout(10, 10)); ????
 
-
-        //Ajouter le panel au frame
+        //Ajouter les elements au frame
         fenetre.add(caseArn);
         fenetre.add(comparer);
         fenetre.add(description);
-        fenetre.add(coms);
         fenetre.pack();
-        fenetre.setSize(600, 600);
+        fenetre.setSize(900, 1200);
         fenetre.setLayout(null);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setVisible(true);
